d1cefb964ed01c8ce2b41882d9d360c1a3497a5b
Hx76 <43670079+Hx76@users.noreply.github.com>
2019-07-11 12:32:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activitylifecycletest/DialogAtivity.java
@@ -0,0 +1,14 @@
+package com.example.activitylifecycletest;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DialogAtivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
